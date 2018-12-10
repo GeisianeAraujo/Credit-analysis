3e3f01036a7df4cef9f81e183938ad88f1623553
Geisiane <geisiane.aa@gmail.com>
2018-12-10 00:56:50 -0200

    Algoritmo de Random forest

---
+++ arvores-decisao-credit-analysis.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  9 23:03:54 2018
+
+@author: GeisyPC
+"""
+
+import pandas as pd
+
+base = pd.read_csv('credit-data.csv')
+base.loc[base.age < 0, 'age'] = 40.92
+               
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(previsores[:, 1:4])
+previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+from sklearn.model_selection  import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
+# Algoritmo de árvore de descisão
+from sklearn.tree import DecisionTreeClassifier
+classificador = DecisionTreeClassifier(criterion='entropy', random_state=0)
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+# Visualização do percentual/quantidade de erros do registos de previsãos 
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
