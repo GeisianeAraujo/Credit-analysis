13677ec27165c12fdeeb28f8a5099e879d078f6a
Geisiane <geisiane.aa@gmail.com>
2018-12-11 22:27:04 -0200

    Algoritmo KNN

---
+++ knn-credit-analysis.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 11 22:05:41 2018
+
+@author: GeisyPC
+"""
+
+import pandas as pd
+
+base = pd.read_csv('credit-data.csv')
+base.loc[base.age < 0, 'age'] = 40.92
+               
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(previsores[:, 1:4])
+previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
+# algoritmo KNN (Aprendizagem baseada em instâncias)
+# Calcula a distância euclidiana
+from sklearn.neighbors import KNeighborsClassifier
+classificador = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p = 2)
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+
+import collections
+collections.Counter(classe_teste)
